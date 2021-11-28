ceec884e922c09021af65d8f49f39ac164736e2d
jinhwansuh <nick02190219@gmail.com>
2021-11-28 15:16:44 +0900

    Chore: 환경설정(craco, lint, prettier)

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+// https://velog.io/@nomore12/CRA-Typescript-eslint-prettier-%EC%85%8B%ED%8C%85%ED%95%98%EA%B8%B0
+
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
+};
