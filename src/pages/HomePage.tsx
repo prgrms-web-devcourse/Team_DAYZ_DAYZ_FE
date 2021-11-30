227026d2c337e1d283997ec5d0f8f9287641f319
chloe41297 <79011228+chloe41297@users.noreply.github.com>
2021-11-30 16:32:20 +0900

    Design: Header 구현

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,23 @@
+import styled from '@emotion/styled';
 import React from 'react';
 
 const HomePage = () => {
-  return <div>HomePage입니다.</div>;
+  return (
+    <HomePageWrapper>
+      <Container>HomePage입니다.</Container>
+    </HomePageWrapper>
+  );
 };
+
+const HomePageWrapper = styled.div`
+  overflow: scroll;
+  width: 100vw;
+  height: 100vh;
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 1000px;
+`;
 
 export default HomePage;
---
+++ src/template/Header/Header.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable react/react-in-jsx-scope */
+import styled from '@emotion/styled';
+import { Bell, ChevronDown } from 'react-feather';
+
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <Title>DAYZ</Title>
+      <Location>
+        <div>서울시 송파구</div>
+        <ChevronDown size={20} color="#f5f5f5" />
+      </Location>
+      <BellIcon>
+        <Bell size={20} color="#f5f5f5" />
+      </BellIcon>
+    </HeaderWrapper>
+  );
+};
+
+const HeaderWrapper = styled.div`
+  position: fix;
+  top: 0px;
+  left: 0px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  width: 100%;
+  height: 95px;
+  background: linear-gradient(135deg, #b88bd6 0%, #b88bd6 0.01%, #a8bac8 100%);
+  position: relative;
+`;
+
+const Title = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  font-size: 30px;
+  font-weight: 700;
+  color: #f5f5f5;
+  margin-bottom: 10px;
+  margin-left: 10px;
+`;
+
+const Location = styled.div`
+  font-size: 16px;
+  font-weight: 700;
+  color: #f5f5f5;
+  margin-bottom: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+const BellIcon = styled.div`
+  position: absolute;
+  bottom: 0;
+  right: 0;
+  margin-bottom: 10px;
+  margin-right: 10px;
+`;
+
+export default Header;
