a5a1d8a7617895550a386300d03bb2f1c5e568aa
chloe41297 <79011228+chloe41297@users.noreply.github.com>
2021-11-30 16:54:38 +0900

    Feat: 선호하는 지역 페이지 연결

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
-import { ExplorePage, HomePage } from '../pages';
+import { ChangeLocation, ExplorePage, HomePage } from '../pages';
 
 const Router = () => {
   return (
     <Switch>
       <Route path="/" exact component={HomePage} />
       <Route path="/explore" exact component={ExplorePage} />
+      <Route path="/location" exact component={ChangeLocation} />
     </Switch>
   );
 };
