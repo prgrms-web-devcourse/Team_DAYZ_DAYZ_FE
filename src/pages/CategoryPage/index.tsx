483100228b586e1740b4c3f363bc76f74bed1bb3
jinhwansuh <nick02190219@gmail.com>
2021-12-02 12:59:59 +0900

    Chore: react-router-dom 사용

--- src/pages/CategoryPage/index.tsx
+++ src/pages/CategoryPage/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router';
+import { useParams } from 'react-router-dom';
 
 const CategoryPage = () => {
   const { genre } = useParams<{ genre: string }>();
