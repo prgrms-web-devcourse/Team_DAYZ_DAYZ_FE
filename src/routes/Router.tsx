8b49eaf79647d94155c4bd4ef0bcdec144734b9f
jinhwansuh <nick02190219@gmail.com>
2021-12-06 17:29:22 +0900

    Rename: 라우팅 수정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -17,32 +17,30 @@ import {
   UserBookedPage,
   UserFollowingPage,
   LeaveCommentPage,
+  UploadProductPage,
+  UserReviewPage,
+  UserProfileEditPage,
 } from '../pages';
-import UploadProductPage from '../pages/UploadProductPage';
-
-import { UserReviewPage } from '../pages/UserPage';
-
-import { UserProfileEditPage } from '../pages/UserPage';
 
 const Router = () => {
   return (
     <Switch>
       <Route path="/" exact component={HomePage} />
-      <Route path="/loginHome" exact component={LoginPage} />
+      <Route path="/login" exact component={LoginPage} />
       <Route path="/category/:genre" exact component={CategoryPage} />
       <Route path="/feed" exact component={FeedPage} />
       <Route path="/search" exact component={SearchPage} />
       <Route path="/products/:id" exact component={ProductsDetailPage} />
       <Route path="/booking/:id" exact component={BookingPage} />
-      <Route path="/user" exact component={UserPage} />
+      <Route path="/user/:id" exact component={UserPage} />
       <Route path="/user/book" exact component={UserBookedPage} />
       <Route path="/user/review" exact component={UserReviewPage} />
       <Route path="/user/following" exact component={UserFollowingPage} />
       <Route path="/user/edit" exact component={UserProfileEditPage} />
       <Route path="/feed/comments/:id" exact component={Comments} />
-      <Route path="/search/results" exact component={SearchResultsPage} />
+      <Route path="/search/:results" exact component={SearchResultsPage} />
       <Route path="/workshop" component={WorkshopPage} />
-      <Route path="/upload" exact component={UploadFeedPage} />
+      <Route path="/upload/feed" exact component={UploadFeedPage} />
       <Route path="/upload/comments/:id" exact component={LeaveCommentPage} />
       <Route path="/upload/products" exact component={UploadProductPage} />
       <Route path="*" component={NotFoundPage} />
