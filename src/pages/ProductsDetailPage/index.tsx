763ee2dcc37e684c7e557f2b700bd851630dfa31
jinhwansuh <nick02190219@gmail.com>
2021-12-07 02:05:41 +0900

    Design: product detail 페이지 디자인 변경

--- src/pages/ProductsDetailPage/index.tsx
+++ src/pages/ProductsDetailPage/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
 import { useParams } from 'react-router-dom';
-import { Modal } from '../../components/base';
+import { Image, Modal, Text } from '../../components/base';
+import { X } from 'react-feather';
 
 const ProductsDetailPage = () => {
   const { id } = useParams<{ id: string }>();
@@ -13,59 +14,107 @@ const ProductsDetailPage = () => {
 
   return (
     <>
-      <div>ProductsDetailPage {id} 입니다.</div>
-      <img src="https://via.placeholder.com/150" />
+      <Image
+        height={'210px'}
+        width={'100%'}
+        src={'https://i.pinimg.com/564x/eb/02/19/eb021934d0750d0b91ad6d6ff3e4ed84.jpg'}
+        alt={'class'}
+        mode={'cover'}
+        placeholder={'https://via.placeholder.com/150'}
+      />
 
       <ProductsDetailContainer>
-        <ProductsNameWrapper>
-          <div>클래스 이름</div>
+        <ProductNameWrapper>
+          <HeaderText>클래스 이름</HeaderText>
           <div>별점 4.0</div>
-        </ProductsNameWrapper>
-        <div>클래스 소개</div>
-        <div>커리큘럼</div>
+        </ProductNameWrapper>
+        <ProductContentWrapper>
+          <HeaderText>클래스 소개</HeaderText>
+          <div> 랩몬스터와 슈가의 음악감상하기 클래스입니다.</div>
+          <div> 랩몬스터와 슈가의 음악감상하기 클래스입니다.</div>
+          <div> 랩몬스터와 슈가의 음악감상하기 클래스입니다.</div>
+        </ProductContentWrapper>
+        <ProductContentWrapper>
+          <HeaderText>커리큘럼</HeaderText>
+          <div> 우선 춤을 배웁니다.</div>
+          <div> 우선 춤을 배웁니다.</div>
+          <div> 우선 춤을 배웁니다.</div>
+        </ProductContentWrapper>
       </ProductsDetailContainer>
 
-      <ProductsReviewContainer>
-        <div>
-          후기<span>999+</span>
-        </div>
+      <ProductReviewContainer>
+        <HeaderText>
+          후기<span>(999+)</span>
+        </HeaderText>
         <div> 우선 그냥 후기(내용, 날짜?)</div>
         <div onClick={() => setVisible(true)}>+ 후기 더보기</div>
-      </ProductsReviewContainer>
+      </ProductReviewContainer>
 
       <AuthorDetailContainer>
-        <div>작가 정보들들...</div>
+        <HeaderText>작가 정보</HeaderText>
         <div>위치 보기</div>
+        {/* 스크롤 되었을 때, 확인을 위해 임시로 넣어둠*/}
+        <input style={{ width: 200, height: 2000 }} />
       </AuthorDetailContainer>
 
       <ReservationContainer>
         <div>가격 45,000</div>
         <ReservationButton>예약하기</ReservationButton>
       </ReservationContainer>
-      <ModalWrapper>
-        <Modal visible={visible} onClose={() => setVisible(false)} width={'100%'} height={'70%'}>
-          <button onClick={() => setVisible(false)}>Close</button>
-          <div>별점</div>
-          <ReviewWrapper>후기1</ReviewWrapper>
+
+      <StyledModal
+        visible={visible}
+        onClose={() => setVisible(false)}
+        width={'100%'}
+        height={'80%'}
+      >
+        <ModalHeader>
+          <StyledButton onClick={() => setVisible(false)}>
+            <X size={40} />
+          </StyledButton>
+        </ModalHeader>
+        <div>별점</div>
+        <ReviewContainer>
+          {/* 리뷰 보여주는 칸은 컴포넌트화 */}
+          <ReviewWrapper>
+            후기1가 길면후기1가 길면후기1가 길면후기1가 길면후기1가 길면후기1가 길면후기1가
+            길면후기1가 길면후기1가 길면후기1가 길면후기1가 길면후기1가 길면후기1가 길면후기1가
+            길면후기1가 길면후기1가 길면후기1가 길면후기1가 길면후기1가 길면후기1가 길면후기1가
+            길면후기1가 길면후기1가 길면후기1가 길면
+          </ReviewWrapper>
           <ReviewWrapper>후기2</ReviewWrapper>
           <ReviewWrapper>후기2</ReviewWrapper>
           <ReviewWrapper>후기2</ReviewWrapper>
           <ReviewWrapper>후기2</ReviewWrapper>
           <ReviewWrapper>후기2</ReviewWrapper>
-        </Modal>
-      </ModalWrapper>
+        </ReviewContainer>
+      </StyledModal>
     </>
   );
 };
+
+// min-height는 우선 어떠한 모양으로 나올지 보는 것이기 때문에 나중에 삭제
 const ProductsDetailContainer = styled.div`
   min-height: 100px;
+  margin: 20px 0;
+  border-bottom: 2px solid black;
 `;
-const ProductsNameWrapper = styled.div`
+const ProductNameWrapper = styled.div`
   display: flex;
   justify-content: space-between;
+  margin: 20px 0;
 `;
-const ProductsReviewContainer = styled.div`
+const ProductContentWrapper = styled.div`
+  margin: 20px 0;
+`;
+const HeaderText = styled(Text)`
+  font-size: 25px;
+  font-weight: 700;
+`;
+const ProductReviewContainer = styled.div`
   min-height: 100px;
+  margin: 20px 0;
+  border-bottom: 2px solid black;
 `;
 const AuthorDetailContainer = styled.div`
   min-height: 100px;
@@ -73,9 +122,14 @@ const AuthorDetailContainer = styled.div`
 const ReservationContainer = styled.div`
   position: fixed;
   width: 100%;
+  max-width: 640px;
   display: flex;
-  bottom: 10px;
+  bottom: 65px;
+  background-color: #aaa;
+  z-index: 100;
+  display: flex;
   justify-content: space-around;
+  align-items: center;
 `;
 const ReservationButton = styled.button`
   height: 56px;
@@ -86,13 +140,30 @@ const ReservationButton = styled.button`
   font-size: 25px;
   line-height: 29px;
 `;
-const ModalWrapper = styled.div`
-  width: 100%;
+const StyledModal = styled(Modal)`
+  border-top-right-radius: 20px;
+  border-top-left-radius: 20px;
+`;
+
+const ModalHeader = styled.div`
+  background: linear-gradient(135deg, #b88bd6 0%, #b88bd6 0.01%, #a8bac8 100%);
+  height: 56px;
+`;
+const StyledButton = styled.button`
+  background: none;
+  border: none;
+  right: 0;
+  position: relative;
+`;
+const ReviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 const ReviewWrapper = styled.div`
-  height: 100px;
-  width: 100%;
-  background-color: blue;
+  min-height: 100px;
+  width: 90%;
+  background-color: lightcyan;
   margin: 20px 0;
 `;
 export default ProductsDetailPage;
