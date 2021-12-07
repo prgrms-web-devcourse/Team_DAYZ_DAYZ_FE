629b844293b0dd461d89f08dc137b1ddf8d1d5e0
jinhwansuh <nick02190219@gmail.com>
2021-12-08 00:04:07 +0900

    Feat: useTimeout hook 추가

---
+++ src/hooks/useTimeoutFn.ts
@@ -0,0 +1,28 @@
+import { TimeHTMLAttributes, useCallback, useEffect, useRef } from 'react';
+
+const useTimeoutFn = (fn: () => void, ms: number) => {
+  const timeoutId = useRef<any>();
+  const callback = useRef(fn);
+
+  useEffect(() => {
+    callback.current = fn;
+  }, [fn]);
+
+  const run = useCallback(() => {
+    if (timeoutId.current) clearTimeout(timeoutId.current);
+
+    timeoutId.current = setTimeout(() => {
+      callback.current();
+    }, ms);
+  }, [ms]);
+
+  const clear = useCallback(() => {
+    if (timeoutId.current) clearTimeout(timeoutId.current);
+  }, []);
+
+  useEffect(() => clear, [clear]);
+
+  return [run, clear];
+};
+
+export default useTimeoutFn;
