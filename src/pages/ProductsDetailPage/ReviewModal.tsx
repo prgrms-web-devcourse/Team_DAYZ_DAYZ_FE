8fb5d966c1cf0e9333955f938807b70873a94dbf
jinhwansuh <nick02190219@gmail.com>
2021-12-07 20:25:02 +0900

    Feat: Modal 분리

--- src/pages/ProductsDetailPage/ReviewModal.tsx
+++ src/pages/ProductsDetailPage/ReviewModal.tsx
@@ -1,31 +1,52 @@
-import styled from '@emotion/styled';
 import React from 'react';
+import styled from '@emotion/styled';
+import { Modal } from '../../components/base';
+import { X } from 'react-feather';
+import { Review } from '../../components/domain';
 
-// interface Props {
-//   setReviewDetails;
-// }
+interface Props {
+  visible: boolean;
+  setVisible: (T: boolean) => void;
+}
 
-const ReviewModal = () => {
+const ReviewModal = ({ visible = false, setVisible }: Props) => {
   return (
-    <ModalWrapper>
-      <Square>리뷰 입니다.</Square>
-    </ModalWrapper>
+    <StyledModal visible={visible} onClose={() => setVisible(false)} width={'100%'} height={'80%'}>
+      {/* 바깥 스크롤 막기 추가 */}
+      <ModalHeader>
+        <StyledButton onClick={() => setVisible(false)}>
+          <X size={40} />
+        </StyledButton>
+      </ModalHeader>
+      <div>별점</div>
+      <ReviewContainer>
+        <Review />
+        <Review />
+        <Review />
+      </ReviewContainer>
+    </StyledModal>
   );
 };
 
-const ModalWrapper = styled.div`
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
+const StyledModal = styled(Modal)`
+  border-top-right-radius: 20px;
+  border-top-left-radius: 20px;
 `;
 
-const Square = styled.div`
-  position: absolute;
-  width: 1000px;
-  height: 300px;
-  background-color: purple;
+const ModalHeader = styled.div`
+  background: linear-gradient(135deg, #b88bd6 0%, #b88bd6 0.01%, #a8bac8 100%);
+  height: 56px;
+  display: flex;
+  justify-content: flex-end;
+`;
+const StyledButton = styled.button`
+  background: none;
+  border: none;
+`;
+const ReviewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 export default ReviewModal;
