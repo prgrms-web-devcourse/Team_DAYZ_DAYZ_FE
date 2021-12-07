2e9eb87a36d1bb8817647a47a5767a12ec49a4e0
Lorraine <limgytjd98@gmail.com>
2021-12-07 16:00:40 +0900

    Refactor: Linkbox 컴포넌트로 대체

--- src/pages/WorkshopPage/ProductsList.tsx
+++ src/pages/WorkshopPage/ProductsList.tsx
@@ -1,37 +1,23 @@
 import styled from '@emotion/styled';
 import React from 'react';
-import { ChevronRight } from 'react-feather';
+import LinkBox from '../../components/domain/LinkBox';
 
 const ProductsList = () => {
   return (
     <ProductsListContainer>
-      <p>총 3개의 클래스</p>
-      <ProductContainer>
-        <img src="https://picsum.photos/id/1/200/300" alt="" />
+      <ProductsCnt>총 3개의 클래스</ProductsCnt>
+      <LinkBox src={'https://picsum.photos/id/1/200/300'}>
         <p>하루동안 방탄되어보기</p>
-        <ChevronRight />
-      </ProductContainer>
+      </LinkBox>
     </ProductsListContainer>
   );
 };
 export default ProductsList;
 
 const ProductsListContainer = styled.div`
-  margin: 24px;
+  margin: 14px;
 `;
 
-const ProductContainer = styled.div`
-  width: 100%;
-  height: 100px;
-  border-radius: 12px;
-  margin-top: 12px;
-  background-color: gray;
-  display: flex;
-  align-items: center;
-  & img {
-    width: 80px;
-    height: 80px;
-    border-radius: 12px;
-    margin: 0 12px;
-  }
+const ProductsCnt = styled.div`
+  margin-bottom: 12px;
 `;
