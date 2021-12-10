978e1fef0e90588a8a348ead12aa34311bdcb22d
jinhwansuh <nick02190219@gmail.com>
2021-12-10 16:53:00 +0900

    Refactor: 리뷰 리스트 더미데이터 적용

--- src/pages/WorkshopPage/ReviewListPage.tsx
+++ src/pages/WorkshopPage/ReviewListPage.tsx
@@ -1,94 +1,82 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Star } from 'react-feather';
-import { Avatar, Image } from '../../components/base';
+import { Avatar, Image, Text } from '../../components/base';
 import { DUMMY_REVIEW_DATA } from './DUMMY_DATA';
 // 1. 전체 리뷰 가져오기 https://backend-devcourse.notion.site/cbf7db08c6ae437b904d594dc92a8219
 
 const ReviewListPage = () => {
+  const { averageScore, reviews } = DUMMY_REVIEW_DATA;
+
   return (
     <>
       <StarContainer>
-        <Star size={36} />
-        {DummyData.data ? <span>{DummyData.data.averageScore}</span> : ''}
+        <StyledStar size={36} />
+        <span>{averageScore}</span>
       </StarContainer>
       <section>
-        <ReviewContainer>
-          <AteliarWrapper>
-            <Avatar
-              lazy
-              src={'https://via.placeholder.com/150'}
-              shape="circle"
-              size={70}
-              alt="profile"
-              placeholder="https://via.placeholder.com/150"
-            />
-            <AteliarDetailWrapper>
-              <div>사용자 123</div>
-              <div>2020-05-23</div>
-            </AteliarDetailWrapper>
-          </AteliarWrapper>
-          <ContentWrapper>
-            내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용내용
-          </ContentWrapper>
-          <ImageContainer>
-            <ImageWrapper>
-              <Image
-                lazy
-                src="https://via.placeholder.com/150"
-                width={'100%'}
-                height={'100%'}
-                alt="contents"
-                mode="fill"
-                style={{ borderRadius: '12px', minWidth: '120px' }}
-              />
-            </ImageWrapper>
-            <ImageWrapper>
-              <Image
-                lazy
-                src="https://via.placeholder.com/150"
-                width={'100%'}
-                height={'100%'}
-                alt="contents"
-                mode="fill"
-                style={{ borderRadius: '12px', minWidth: '120px' }}
-              />
-            </ImageWrapper>
-            <ImageWrapper>
-              <Image
-                lazy
-                src="https://via.placeholder.com/150"
-                width={'100%'}
-                height={'100%'}
-                alt="contents"
-                mode="fill"
-                style={{ borderRadius: '12px', minWidth: '120px' }}
-              />
-            </ImageWrapper>
-            <ImageWrapper>
-              <Image
-                lazy
-                src="https://via.placeholder.com/150"
-                width={'100%'}
-                height={'100%'}
-                alt="contents"
-                mode="fill"
-                style={{ borderRadius: '12px', minWidth: '120px' }}
-              />
-            </ImageWrapper>
-          </ImageContainer>
-        </ReviewContainer>
+        {reviews.length
+          ? reviews.map(
+              ({
+                reviewId,
+                createdAt,
+                title,
+                content,
+                score,
+                images,
+                member: { memberId, name, imageUrl },
+              }) => (
+                <ReviewContainer key={reviewId}>
+                  <AteliarWrapper>
+                    <Avatar
+                      lazy
+                      src={imageUrl}
+                      shape="circle"
+                      size={70}
+                      alt="profile"
+                      placeholder="https://via.placeholder.com/150"
+                    />
+                    <AteliarDetailWrapper>
+                      <StyledText>{name}</StyledText>
+                      <StyledText>{createdAt}</StyledText>
+                    </AteliarDetailWrapper>
+                  </AteliarWrapper>
+                  <ContentWrapper>{content}</ContentWrapper>
+                  <ImageContainer>
+                    {images.length
+                      ? images.map(({ imageUrl, sequence }) => (
+                          <ImageWrapper key={sequence}>
+                            <Image
+                              lazy
+                              src={imageUrl}
+                              width={'100%'}
+                              height={'100%'}
+                              alt="contents"
+                              mode="fill"
+                              style={{ borderRadius: '12px', minWidth: '120px' }}
+                            />
+                          </ImageWrapper>
+                        ))
+                      : ''}
+                  </ImageContainer>
+                </ReviewContainer>
+              ),
+            )
+          : '리뷰가 없어요 ㅠ'}
       </section>
     </>
   );
 };
-export default ReviewListPage;
 
 const StarContainer = styled.div`
   margin: 24px 36px;
   display: flex;
   align-items: center;
   font-size: 36px;
+`;
+
+const StyledStar = styled(Star)`
+  margin-right: 20px;
 `;
 
 const ReviewContainer = styled.div`
@@ -109,7 +97,11 @@ const AteliarWrapper = styled.div`
 const AteliarDetailWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  margin-left: 30px;
+  margin-left: 20px;
+`;
+
+const StyledText = styled(Text)`
+  margin: 5px 0;
 `;
 
 const ContentWrapper = styled.div`
@@ -135,33 +127,4 @@ const ImageWrapper = styled.div`
   margin-right: 10px;
 `;
 
-// const ReviewContainer = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   border-top: 1px solid black;
-//   & .review {
-//     margin: 24px;
-//     & .profile {
-//       display: flex;
-//       align-items: center;
-//       & img {
-//         width: 60px;
-//         height: 60px;
-//         border-radius: 30px;
-//         margin-right: 12px;
-//       }
-//     }
-//     & .content {
-//       font-size: 24px;
-//       margin: 12px 0;
-//     }
-//     & .photos {
-//       & img {
-//         width: 100px;
-//         height: 100px;
-//         border-radius: 12px;
-//         margin-right: 12px;
-//       }
-//     }
-//   }
-// `;
+export default ReviewListPage;
