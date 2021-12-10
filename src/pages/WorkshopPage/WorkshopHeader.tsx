e93b17d95e42485b1b3a652f2bbade3cca192ec6
jinhwansuh <nick02190219@gmail.com>
2021-12-10 18:25:15 +0900

    Design: Header 디자인 변경

--- src/pages/WorkshopPage/WorkshopHeader.tsx
+++ src/pages/WorkshopPage/WorkshopHeader.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Avatar } from '../../components/base';
-import { Settings, Layout, Calendar, Star } from 'react-feather';
-import { Link } from 'react-router-dom';
 import styled from '@emotion/styled';
+import { Link } from 'react-router-dom';
+import { Settings, Layout, Calendar, Star } from 'react-feather';
+import { Avatar } from '../../components/base';
 import { SETTING, WORKSHOP, PRODUCTSLIST, REVIEWLIST } from '.';
 import { DUMMY_ATELIER_DATA } from './DUMMY_DATA';
+
 // 1. 공방 상세정보 조회 https://backend-devcourse.notion.site/2158d2bf8bab4792baea605062c15d69
+// TODO:
+// 공방 정보가 없으면 뒤로가기 or 메인페이지로 가기
 
 const WorkshopHeader = () => {
   const { intro, name, imageUrl, address, callNo, startTime, endTime } = DUMMY_ATELIER_DATA;
   const ICON_SIZE = 26;
   return (
-    <Container>
+    <>
       <WorkshopProfile>
         <Avatar
           size={80}
@@ -20,7 +23,7 @@ const WorkshopHeader = () => {
           src={imageUrl}
           placeholder={'https://via.placeholder.com/150'}
         />
-        <div className="info">
+        <ContentsWrapper>
           <span>{name}</span>
           <span>{intro}</span>
           <span>{address}</span>
@@ -28,7 +31,7 @@ const WorkshopHeader = () => {
           <span>
             {startTime} - {endTime}
           </span>
-        </div>
+        </ContentsWrapper>
         <Link to={SETTING} style={{ textDecoration: 'none', color: 'black' }}>
           <Settings />
         </Link>
@@ -44,32 +47,29 @@ const WorkshopHeader = () => {
           <Star size={`${ICON_SIZE}px`} />
         </Link>
       </Tabs>
-    </Container>
+    </>
   );
 };
 export default WorkshopHeader;
-
-const Container = styled.div``;
 
 const WorkshopProfile = styled.section`
   display: flex;
   justify-content: space-around;
   align-items: center;
   margin-bottom: 36px;
-  & .profileImg {
-    width: 60px;
-    height: 60px;
-    border-radius: 30px;
+`;
+
+const ContentsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 18px;
+  & span:first-of-type {
+    font-size: 24px;
+    font-weight: 600;
+    margin-bottom: 12px;
   }
-  & .info {
-    display: flex;
-    flex-direction: column;
-    font-size: 18px;
-    & span:first-of-type {
-      font-size: 24px;
-      font-weight: 600;
-      margin-bottom: 12px;
-    }
+  & span:not(first-of-type) {
+    margin: 2px 0;
   }
 `;
 
