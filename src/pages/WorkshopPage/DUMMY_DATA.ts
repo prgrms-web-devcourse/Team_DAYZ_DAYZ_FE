d42836e0fed7e2ad8ff7f70316931424b00bfb36
jinhwansuh <nick02190219@gmail.com>
2021-12-10 15:01:18 +0900

    Feat: 더미데이터 추가

---
+++ src/pages/WorkshopPage/DUMMY_DATA.ts
@@ -0,0 +1,34 @@
+export const DUMMY_ATELIER_DATA = {
+  atelierId: 1,
+  intro: '내 공방을 소개하지',
+  name: '이구역최강자',
+  imageUrl: 'https://via.placeholder.com/150',
+  address: '서울시 송파구 송파로 23길 1',
+  callNo: '02-111-2222',
+  startTime: '10:00',
+  endTime: '22:00',
+};
+
+export const DUMMY_FEED_DATA = {
+  totalCount: 20,
+  pageIndex: 0,
+  pageSize: 10,
+  hasNext: true,
+  post: [
+    {
+      postId: 1,
+      imageUrl: '',
+      createdAt: '',
+    },
+    {
+      postId: 1,
+      imageUrl: '',
+      createdAt: '',
+    },
+    {
+      postId: 1,
+      imageUrl: '',
+      createdAt: '',
+    },
+  ],
+};
