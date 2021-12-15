b4f48f95377a89702f4bb42114c2e03be2393390
jinhwansuh <nick02190219@gmail.com>
2021-12-15 18:57:43 +0900

    Style: Atom type 분리

---
+++ src/atoms/types.ts
@@ -0,0 +1,17 @@
+export interface IUser {
+  id: number;
+  token: string;
+  name: string;
+  profileImageUrl: string;
+  cityId: number;
+  cityName: string;
+  regionId: number;
+  regionName: string;
+  auth: string;
+  atelierId: number;
+}
+
+export interface INavigation {
+  topNavigation: boolean;
+  bottomNavigation: boolean;
+}
