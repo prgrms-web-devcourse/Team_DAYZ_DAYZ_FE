a0cf351f058e72e5bc2e2a01a26a9427217cea81
jinhwansuh <nick02190219@gmail.com>
2021-12-16 23:48:07 +0900

    Feat: 공방 클래스 목록 불러오기 api 추가

--- src/utils/api/dayzApi.ts
+++ src/utils/api/dayzApi.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { API_METHOD } from '../../constants/apiConstant';
-import { Email, Location } from './types';
+import { AtelierClass, Email, Location } from './types';
 
 const axiosInstance = axios.create();
 
@@ -28,10 +28,12 @@ export const requiredtoken = (token: string) => {
     method: API_METHOD.GET,
     url: `/main`,
     headers: {
-      authorization: `Bearer ${token}`,
+      Authorization: token,
     },
   });
 };
+const token =
+  'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJwcm92aWRlcklkIjoiMjAxOTk0ODQ5MSIsInJvbGVzIjpbIlJPTEVfVVNFUiJdLCJpc3MiOiJkYXl6IiwiaWQiOjEsImV4cCI6MTYzOTcxMDg4OSwiaWF0IjoxNjM5NjUwODg5LCJ1c2VybmFtZSI6Iuq5gOyngO2biCJ9.nbpdnrbHM2T7mpuwk2Iif_ekPxMBN5fqcFxVne0R546Ix2k6kEMt-F53Wc01q1wVG7wtrMwT3TyGOYLE2sYCpw';
 
 export const setImageUpload = (data: File) => {
   const formData = new FormData();
@@ -42,7 +44,7 @@ export const setImageUpload = (data: File) => {
     url: `/api/v1/images`,
     data: formData,
     headers: {
-      // Authorization: token,
+      Authorization: token,
       'Content-Type': `multipart/form-data`,
     },
   });
@@ -52,7 +54,7 @@ export const fetchLocationList = (token: string) => {
     method: API_METHOD.GET,
     url: '/api/v1/addresses',
     headers: {
-      authorization: `Bearer ${token}`,
+      Authorization: token,
     },
   });
 };
@@ -62,11 +64,33 @@ export const setLocation = ({ token, cityId, regionId }: Location) => {
     method: API_METHOD.POST,
     url: '/api/v1/members/address',
     headers: {
-      Authorization: `Bearer ${token}`,
+      Authorization: token,
     },
     data: {
       cityId,
       regionId,
+    },
+  });
+};
+
+export const getAtelierClasses = ({
+  atelierId,
+  pageIndex,
+  pageSize,
+  sort,
+  token,
+}: AtelierClass) => {
+  return request({
+    method: API_METHOD.GET,
+    url: `api/v1/classes/ateliers/${atelierId}`,
+    headers: {
+      Authorization: token,
+    },
+    data: {
+      atelierId,
+      pageIndex,
+      pageSize,
+      sort,
     },
   });
 };
--- src/utils/api/types.ts
+++ src/utils/api/types.ts
@@ -1,3 +1,7 @@
+export interface Token {
+  token: string;
+}
+
 export interface Email {
   email: string;
   password: string;
@@ -6,4 +10,15 @@ export interface Location {
   token: string;
   cityId: number;
   regionId: number;
+}
+
+type sortType = {
+  column: string;
+  order: string;
+};
+export interface AtelierClass extends Token {
+  atelierId: number;
+  pageIndex: number;
+  pageSize: number;
+  sort: sortType;
 }
