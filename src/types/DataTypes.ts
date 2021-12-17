e772c1235bf70be093d6e8968b1fc1903bec1e19
jinhwansuh <nick02190219@gmail.com>
2021-12-17 15:53:14 +0900

    Refactor: 공통 type 분리

---
+++ src/types/DataTypes.ts
@@ -0,0 +1,19 @@
+export type TimeData = {
+  date: string;
+  startTime: string;
+  closeTime: string;
+};
+
+export type InputData = {
+  name: string;
+  categoryId: number;
+  intro: string;
+  step1: string;
+  step2: string;
+  step3: string;
+  pickedDate: Date;
+  requiredTime: number;
+  startTime: string;
+  maxPeopleNumber: number;
+  price: number;
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,2 @@
 export type { ImageObject } from './ImageType';
+export type { InputData, TimeData } from './DataTypes';
