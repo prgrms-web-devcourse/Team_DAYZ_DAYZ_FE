1846e20afcb3a7949067b9816a3968be87c7d919
jinhwansuh <nick02190219@gmail.com>
2021-12-17 23:10:06 +0900

    Refactor: Auth의 상태값을 이용하여 privateroute 설정
    authUser가 true가 되야 접근 가능하도록

--- src/routes/PrivateRoute/index.tsx
+++ src/routes/PrivateRoute/index.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-import { getSessionStorageItem } from '../../utils/functions';
+import { useRecoilValue } from 'recoil';
+import { authUserState } from '../../atoms';
 
 interface IProps {
   component: any;
 }
 
 const PrivateRoute = ({ component: Component, ...rest }: IProps) => {
-  // 토큰이 있다면 접근 가능 토근이 없다면 접근 불가.
-  // 이름 바꿔야함
-  const isLogin = getSessionStorageItem('somethingToken', '');
+  const authUser = useRecoilValue(authUserState);
 
   return (
     <Route
       {...rest}
-      render={(props) => (isLogin ? <Component {...props} /> : <Redirect to="/" />)}
+      render={(props) => (authUser ? <Component {...props} /> : <Redirect to="/" />)}
     />
   );
 };
