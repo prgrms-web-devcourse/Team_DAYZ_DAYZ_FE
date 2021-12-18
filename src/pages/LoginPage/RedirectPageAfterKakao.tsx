00376a74577a9d31085df85dd7ace15845eb3f3c
lorraine98 <hyosung0425@naver.com>
2021-12-19 00:51:48 +0900

    Fix: redirectPageAfterKakao 쿼리스트링으로 받는 값 한 번만 호출

--- src/pages/LoginPage/RedirectPageAfterKakao.tsx
+++ src/pages/LoginPage/RedirectPageAfterKakao.tsx
@@ -5,47 +5,40 @@ import { navigationState, userState } from '../../atoms';
 const RedirectPageAfterKakao = () => {
   const setNavigationState = useSetRecoilState(navigationState);
   const resetPageState = useResetRecoilState(navigationState);
+  const setUserState = useSetRecoilState(userState);
+  const history = useHistory();
+  const PATH_AUTHOR_INFO = '/signup/author-info';
+  const PATH_CHECK_LOCATION = '/signup/check-location';
+
   useEffect(() => {
     setNavigationState((prev) => ({
       ...prev,
       topNavigation: false,
       bottomNavigation: false,
     }));
+
+    const searchParams = new URL(window.location.href).searchParams;
+    const isFillAddressInfo = searchParams.get('addressInfoFlag') === 'true';
+    const isFillAuthorInfo = searchParams.get('atelierInfoFlag') === 'true';
+    const role = searchParams.get('auth');
+    const token = searchParams.get('token') ?? '';
+    setUserState((oldUserState) => ({ ...oldUserState, token }));
+    switch (role) {
+      case 'ROLE_USER':
+        isFillAddressInfo ? history.push('/') : history.push(PATH_CHECK_LOCATION);
+        break;
+      case 'ROLE_ATELIERS':
+        if (isFillAuthorInfo && isFillAddressInfo) history.push('/');
+        else if (!isFillAuthorInfo) history.push(PATH_AUTHOR_INFO);
+        else if (!isFillAddressInfo) history.push(PATH_CHECK_LOCATION);
+        break;
+    }
     return () => {
       resetPageState();
     };
   }, []);
 
-  const history = useHistory();
-  const PATH_AUTHOR_INFO = '/signup/author-info';
-  const PATH_CHECK_LOCATION = '/signup/check-location';
-
-  const token = new URL(window.location.href).searchParams.get('token') ?? '';
-  const isFillAddressInfo =
-    new URL(window.location.href).searchParams.get('addressInfoFlag') === 'true';
-  const isFillAuthorInfo =
-    new URL(window.location.href).searchParams.get('atelierInfoFlag') === 'true';
-  const role = new URL(window.location.href).searchParams.get('auth');
-  const setUserState = useSetRecoilState(userState);
-  //todo: token 안들어왔을 때 예외처리
-  setUserState((oldUserState) => ({ ...oldUserState, token }));
-
-  switch (role) {
-    case 'ROLE_USER':
-      isFillAddressInfo ? history.push('/') : history.push(PATH_CHECK_LOCATION);
-      break;
-    case 'ROLE_ATELIERS':
-      if (isFillAuthorInfo && isFillAddressInfo) history.push('/');
-      else if (!isFillAuthorInfo) history.push(PATH_AUTHOR_INFO);
-      else if (!isFillAddressInfo) history.push(PATH_CHECK_LOCATION);
-      break;
-  }
-
-  return (
-    <>
-      <span>잠시만 기다려 주세요</span>
-    </>
-  );
+  return <span>잠시만 기다려 주세요</span>;
 };
 
 export default RedirectPageAfterKakao;
