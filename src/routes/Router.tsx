023cb6e6a6783a7d08e63afa537b21e2f8a2fa16
lorraine98 <hyosung0425@naver.com>
2021-12-19 22:47:04 +0900

    Fix: PrivateRoute 형태 변경

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -30,60 +30,24 @@ import { PrivateRoute } from './CustomRoutes';
 const Router = () => {
   return (
     <Switch>
-      <PrivateRoute exact path="/">
-        <HomePage />
-      </PrivateRoute>
-      <PrivateRoute path="/category/:genre" exact>
-        <CategoryPage />
-      </PrivateRoute>
-      <PrivateRoute path="/feed" exact>
-        <FeedPage />
-      </PrivateRoute>
-      <PrivateRoute path="/search" exact>
-        <SearchPage />
-      </PrivateRoute>
-      <PrivateRoute path="/workshop/:id">
-        <WorkshopPage />
-      </PrivateRoute>
-      <PrivateRoute path="/products/:id" exact>
-        <ProductsDetailPage />
-      </PrivateRoute>
-      <PrivateRoute path="/booking/success" exact>
-        <SuccessBookPage />
-      </PrivateRoute>
-      <PrivateRoute path="/booking/:id" exact>
-        <BookingPage />
-      </PrivateRoute>
-      <PrivateRoute path="/user/book" exact>
-        <UserBookedPage />
-      </PrivateRoute>
-      <PrivateRoute path="/user/review" exact>
-        <UserReviewPage />
-      </PrivateRoute>
-      <PrivateRoute path="/user/following" exact>
-        <UserFollowingPage />
-      </PrivateRoute>
-      <PrivateRoute path="/user/edit" exact>
-        <UserProfileEditPage />
-      </PrivateRoute>
-      <PrivateRoute path="/user/:id" exact>
-        <UserPage />
-      </PrivateRoute>
-      <PrivateRoute path="/feed/comments/:id" exact>
-        <CommentsPage />
-      </PrivateRoute>
-      <PrivateRoute path="/search/:results" exact>
-        <SearchResultsPage />
-      </PrivateRoute>
-      <PrivateRoute path="/upload/feed" exact>
-        <UploadFeedPage />
-      </PrivateRoute>
-      <PrivateRoute path="/upload/comments/:id" exact>
-        <UploadCommentPage />
-      </PrivateRoute>
-      <PrivateRoute path="/upload/products" exact>
-        <UploadProductPage />
-      </PrivateRoute>
+      <PrivateRoute path="/" exact component={HomePage} />
+      <PrivateRoute path="/category/:genre" exact component={CategoryPage} />
+      <PrivateRoute path="/feed" exact component={FeedPage} />
+      <PrivateRoute path="/search" exact component={SearchPage} />
+      <PrivateRoute path="/workshop/:id" exact component={WorkshopPage} />
+      <PrivateRoute path="/products/:id" exact component={ProductsDetailPage} />
+      <PrivateRoute path="/booking/success" exact component={SuccessBookPage} />
+      <PrivateRoute path="/booking/:id" exact component={BookingPage} />
+      <PrivateRoute path="/user/book" exact component={UserBookedPage} />
+      <PrivateRoute path="/user/review" exact component={UserReviewPage} />
+      <PrivateRoute path="/user/following" exact component={UserFollowingPage} />
+      <PrivateRoute path="/user/edit" exact component={UserProfileEditPage} />
+      <PrivateRoute path="/user/:id" exact component={UserPage} />
+      <PrivateRoute path="/feed/comments/:id" exact component={CommentsPage} />
+      <PrivateRoute path="/search/:results" exact component={SearchResultsPage} />
+      <PrivateRoute path="/upload/feed" exact component={UploadFeedPage} />
+      <PrivateRoute path="/upload/comments/:id" exact component={UploadCommentPage} />
+      <PrivateRoute path="/upload/products" exact component={UploadProductPage} />
       <Route path="/login" component={LoginPage} />
       <Route path="/redirect-after-kakao" component={RedirectPageAfterKakao} />
       <Route path="/signup/author-info" component={SignupAuthorInfo} />
